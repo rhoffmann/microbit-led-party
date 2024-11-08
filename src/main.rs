728e88ce58cd6a8c6ff4ef0df3e0d8fde817506b
Richard Hoffmann <rhoffmann@fastmail.com>
2024-11-08 13:18:37 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,25 @@
 use cortex_m_rt::entry;
 use microbit::board::Board;
 use microbit::hal::prelude::*;
-use panic_halt as _;
+use microbit::hal::timer::Timer;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
     board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut row1 = board.display_pins.row1;
 
-    loop {}
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1000_u16);
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1000_u16);
+    }
 }
